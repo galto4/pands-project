fbb4560ba871f02cf280e5d15bfa9974ecf1c2a5
Mark Gallagher <G00439347@atu.ie>
2024-05-16 22:55:15 +0100

    Working on the text file summary and creating messages once the images are created

---
+++ analyze.py
@@ -0,0 +1,46 @@
+# Introduction
+
+# The Fisher Iris data set was introduced by biologist Sir Ronald Aylmer Fisher 
+# in 1936 as an example of discriminant analysis. 
+# It contains 50 samples from each of three species of Iris flowers 
+# (Iris Setosa, Iris Virginica and Iris Versicolor).
+
+# Four features were measured (from each of the samples); the length and width of sepal 
+# and petal (in cm). Based on the combination of the four features, 
+# Fisher developed a linear discriminant model to distinguish the species from one 
+# another.
+
+##########################################################################
+
+# Section 1: Import Libraries
+
+# We will be importing the following libraries for my exploratory analysis:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+##########################################################################
+
+# Section 2: Load the Data
+
+# 2.1. Read the CSV file from a URL 
+iris = pd.read_csv('data.csv') # CSV file created and data loaded directly
+
+# 2.2. Present the first 5 rows for validation
+iris.head()
+
+##########################################################################
+
+# Section 3: Inspect the Data
+
+
+
+##########################################################################
+
+# Section 4: 
+
+
+##########################################################################
+
+# Commands to run each query
